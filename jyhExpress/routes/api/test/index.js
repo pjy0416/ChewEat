2d42f54dba694729833afc57e6e45a24f95ecd9b
dimpl@JinYoung <dimple0416@naver.com>
2018-11-16 20:24:24 +0900

    set-up a dist file to a static and modify codes on routes

--- jyhExpress/routes/api/test/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import {connectCall,transactionCall} from './controller.js';
-let router = express.Router();
-
-router.get('/con',connectCall);
-router.get('/trans', transactionCall);
-
-module.exports = router;
