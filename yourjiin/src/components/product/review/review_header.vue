8df2187c05d0079780bb07b473ec77952f78afb1
DESKTOP-JIU76K2\chj <guswo5580@gmail.com>
2018-11-18 22:26:06 +0900

    Front 3-2
    front 세부 수정

---
+++ yourjiin/src/components/product/review/review_header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class = "review-header">
+        REVIEW
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .review-header {
+        text-align: center;
+        font-size: 1.8rem;
+        font-weight: 900;
+        margin-bottom : 30px;
+    }
+
+    @media(max-width : 500px) {
+        .review-header {
+            font-size : 1.3em;
+        }
+    }
+</style>
