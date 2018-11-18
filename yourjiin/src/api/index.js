f3d6481e77645a11204e5ae57a2f121a6cb0a43a
DESKTOP-JIU76K2\chj <guswo5580@gmail.com>
2018-11-19 00:31:18 +0900

    server 두 번째
    두 번쨰

---
+++ yourjiin/src/api/index.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const api = {
+    product : '/api/text/query2',
+};
+
+function fetchProduct(){
+    return axios.get(api.product);
+}
+
+function fetchAsk(){
+    return axios.get(api.ask);
+}
+
+function fetchJobs(){
+    return axios.get(api.jobs);
+}
+//axios에 대한 부분을 api 속에서 선언해주고 각 views에서 가져갈 수 있게끔 해준다
+function fetchUser(){
+    return axios.get(api.user);
+}
+function fetchItem(){
+    return axios.get(api.item);
+}
+
+export {
+    fetchProduct,
+    fetchAsk,
+    fetchJobs,
+    fetchUser,
+    fetchItem
+}
